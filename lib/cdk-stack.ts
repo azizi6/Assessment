e680cd4dd2f78fef9cd6e52b38719acbbf8b88f2
fatemehazizi <Fatemeh.Azizi@customerservice.nsw.gov.au>
2023-09-25 13:58:53 +1000

    Initial commit

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Function, Runtime, Code, FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+     // lambda function
+     const myFunction = new Function(this, "axiosLambdaHandler", {
+      runtime: Runtime.NODEJS_14_X,
+      functionName:'testasses',
+      code: Code.fromAsset('./src/asess.zip'),
+      memorySize: 1024,
+      handler: "index.handler",
+    
+    });
+
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+      }
+    });
+
+    new CfnOutput(this, 'FunctionUrl', {
+      value: myFunctionUrl.url,
+    });
+
+
   }
 }
